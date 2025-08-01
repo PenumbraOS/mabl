39ecec3e3c85fd289e9b993fa814a41c730b2c10
Adam Gastineau <adam@agg.im>
2025-07-31 19:15:40 -0700

    Set up as launcher app

--- build.sh
+++ build.sh
@@ -3,4 +3,12 @@ if ! [ -f mabl/src/main/assets/minilm-l6-v2-qint8-arm64.onnx ]; then
 fi
 ./gradlew :plugins:demo:installDebug :plugins:aipinsystem:installDebug :plugins:system:installDebug :plugins:openai:installDebug :mabl:installAipinDebug
 adb shell appops set com.penumbraos.plugins.openai MANAGE_EXTERNAL_STORAGE allow
+adb shell pm disable-user --user 0 humane.experience.systemnavigation
+sleep 1
+adb shell cmd package set-home-activity com.penumbraos.mabl/.MainActivity
+sleep 1
+# I think one of these works
+adb shell settings put secure launcher_component com.penumbraos.mabl/.MainActivity
+adb shell settings put system home_app com.penumbraos.mabl
+adb shell settings put global default_launcher com.penumbraos.mabl/.MainActivity
 echo "Built on $(date '+%Y-%m-%d %H:%M:%S')"
